65c424d0f6e707046b45923d6f2bc9dbfa95b533
Peter Jones <pjxcvi@proton.me>
2023-05-06 12:57:48 -0400

    add

---
+++ mapParks.js
@@ -0,0 +1,9 @@
+const parks = [
+    { name: "Biscayne", rating: 4.2 },
+    { name: "Grand Canyon", rating: 5 },
+    { name: "Gateway Arch", rating: 4.5 },
+    { name: "Indiana Dunes", rating: 4.1 },
+  ];
+
+  const result = parks.map((parks) => `${parks.name}: ${parks.rating}`);
+  console.log(result);
