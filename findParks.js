7ecf7866c509889749ff175d4890360bdb7b1eb5
Peter Jones <pjxcvi@proton.me>
2023-05-05 23:21:01 -0400

    add findPark function

---
+++ findParks.js
@@ -0,0 +1,9 @@
+const parks = [
+    { name: "Biscayne", rating: 4.2 },
+    { name: "Grand Canyon", rating: 5 },
+    { name: "Gateway Arch", rating: 4.5 },
+    { name: "Indiana Dunes", rating: 4.1 },
+  ];
+
+  let found = parks.find((park) => park.rating === 4.1); 
+  console.log(found);
