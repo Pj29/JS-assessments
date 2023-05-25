283aa0ec356efebdc39e3c0c950e47c310ce1b43
Peter Jones <pjxcvi@proton.me>
2023-05-25 01:52:03 -0400

    add files

--- promisechain.js
+++ promisechain.js
@@ -1,15 +1,34 @@
+const axios = require("../utils/axios");
+const BASE_URL = "http://localhost:5000";
+
+async function updateIfExists(id, body) {
+  const url = `${BASE_URL}/constellations/${id}`;
+  
+  try {
+    await axios.get(url);
+    const { data } = await axios.put(url, body);
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+module.exports = {
+  updateIfExists,
+};
+
 new Promise ((resolve, reject) => {
-    const random = Math.floor(Math.random() * 10);
-    random > 5 ? resolve(random) : reject(random);
+  const random = Math.floor(Math.random() * 10);
+  random > 5 ? resolve(random) : reject(random);
 })
-    .then((response) => {
-        console.log("Resolved!", response);
-        return response;
-    })
-        .catch((response) => {
-            console.log("Rejected!", response);
-            return response;
-        })
-        .then((response) => {
-            console.log("Add 10", response + 10);
-        });
+  .then((response) => {
+      console.log("Resolved!", response);
+      return response;
+  })
+      .catch((response) => {
+          console.log("Rejected!", response);
+          return response;
+      })
+      .then((response) => {
+          console.log("Add 10", response + 10);
+      });
